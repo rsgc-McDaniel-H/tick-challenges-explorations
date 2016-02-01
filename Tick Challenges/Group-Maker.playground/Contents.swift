db32df993c3d615294d891653ac1162193bebc89
rsgc-McDaniel-H <hmcdaniel@rsgc.on.ca>
2016-01-31 22:28:40 -0500

    In class random group genorator

--- Tick Challenges/Group-Maker.playground/Contents.swift
+++ Tick Challenges/Group-Maker.playground/Contents.swift
@@ -2,6 +2,11 @@
 
 import Cocoa
 
+//spesification
+//1. create groups from the list givin 
+//2. Groups should be randomized 
+
+
 //Matthew Anderson
 //Stuart Armstrong
 //Puneet Bagga
@@ -26,5 +31,57 @@ import Cocoa
 //Carlos Noble Curveira
 //Matthew Pritchard
 //Sean Trevor
+
+//creat data structures 
+//Note: Data structures should be named using camalCasing
+var studentList = [String]()
+studentList.append("Matthew Anderson")
+studentList.append("Stuart Armstrong")
+studentList.append("Puneet Bagga")
+studentList.append("Noah Barney")
+studentList.append("Scott Blackwell")
+studentList.append("Mattias Blum")
+studentList.append("Jeremy Burleton")
+studentList.append("Liam Byrne")
+studentList.append("Gilbert Chien")
+studentList.append("Thomas Davies")
+studentList.append("Maxym Dubczak")
+studentList.append("Andrew Elder")
+studentList.append("Oliver Gane")
+studentList.append("Jeffrey Goldsmith")
+studentList.append("William Graham")
+studentList.append("Michael Green")
+studentList.append("Nicholas Jones")
+studentList.append("Brendan Leder")
+studentList.append("Alex Lo")
+studentList.append("Mark McCutcheon")
+studentList.append("Hudson McDaniel")
+studentList.append("Robert Miller")
+studentList.append("Carlos Noble Curveira")
+studentList.append("Matthew Pritchard")
+studentList.append("Sean Trevor")
+
+//build a parellel array that has random values
+var studentRandomPosition = [Int]()
+//for i in 1...25{
+//print("\(i)")
+//studentRandomPosition.append(Int(arc4random_uniform(1000)))
+//}
+//we have illustrated a common run time error out of bounds because we ran over the end of the array
+
+//see whats there
+//studentRandomPosition
+
+//Find the highest number in the list of random numbers 
+for i in 1...25{
+//    print("The current value is: \(studentRandomPosition[i])')")
+//}
+
+
+
+for (index, value) in studentRandomPosition.enumerate(){
+    print("The index is \(index) and the value is \(value)")
+}
+
 
 var str = "Hello, playground"
