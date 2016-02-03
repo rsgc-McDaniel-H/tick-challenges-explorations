a6b752595b77d8fd06362cd7e1c319c7aa93b1ad
rsgc-McDaniel-H <hmcdaniel@rsgc.on.ca>
2016-02-02 23:06:11 -0500

    Tried to get it to work but failed

---
+++ Tick Challenges/Tick Challenges/Alien.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var eyes = 0
+var antena = 0
+
+var TroyMartian = eyes = 4; antena = 3;
+print(eyes,antena);
+
+if((eyes = 4)((antena = 3)){
+    print(TroyMartian);
+};
+} else { print(,?);
