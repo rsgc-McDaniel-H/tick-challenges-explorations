1795ece42ba13cd892ce4ec2a790d01053c23872
rsgc-McDaniel-H <hmcdaniel@rsgc.on.ca>
2016-02-04 22:36:30 -0500

    Continued work on alien

--- Tick Challenges/Tick Challenges/Alien.playground/Contents.swift
+++ Tick Challenges/Tick Challenges/Alien.playground/Contents.swift
@@ -8,10 +8,16 @@ var str = "Hello, playground"
 var eyes = 0
 var antena = 0
 
+var Alien#2 = eyes = 6; antena = 5;
 var TroyMartian = eyes = 4; antena = 3;
+
 print(eyes,antena);
 
 if((eyes = 4)((antena = 3)){
     print(TroyMartian);
 };
 } else { print(,?);
+
+
+
+
